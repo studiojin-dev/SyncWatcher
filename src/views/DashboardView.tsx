cc9f4fdab18533667b72909fd6f6da849fae5d30
Kim Jeongjin <kimjj81@gmail.com>
2026-02-09 10:37:40 +0900

    Update app icon assets and include current workspace changes

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -76,7 +76,7 @@ function DashboardView() {
                     LOADING_SYSTEM_MODULES...
                 </div>
             ) : (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 gap-8">
                     {volumes.map((volume, index) => (
                         <CardAnimation key={volume.mount_point} index={index}>
                             <VolumeCard volume={volume} />
